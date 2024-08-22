c927ce4163384f71891bfe4ebc6502836d932280
Gustavo Santacruz <gustavosantacruz17@hotmail.com>
2024-08-21 19:31:18 -0500

    abordamos inputs

---
+++ inputs/inputs.py
@@ -0,0 +1,5 @@
+#Pedirle un dato al usuario
+nombre = input("Cual es tu nombre: ")
+#mostrando el dato
+print(f'el nombre es {nombre}')
+print(type(nombre))
---
+++ inputs/inputs_numeros.py
@@ -0,0 +1,4 @@
+#pedir un dato un numero
+numero = input("dame un numero")
+resultado=int(numero)*2
+print(resultado)
