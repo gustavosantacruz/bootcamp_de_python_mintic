92eda62fe917e95064a2cce1405c384599cfda22
Gustavo Santacruz <gustavosantacruz17@hotmail.com>
2024-09-02 20:03:33 -0500

    uso de funciones

---
+++ uso-de-funciones.py
@@ -0,0 +1,5 @@
+def sakura(name, last_name= "no tiene apellido"):
+    print("hola", name, last_name)
+sakura("gustavo", "santacruz")
+sakura("santacruz")
+sakura(last_name="santacruz", name= "gustavo")
